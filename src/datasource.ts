ef05c51576c09f280b11508ef0042e1a989e967a
Eduardo Cruz Araujo <eduardo.araujo@carefy.com.br>
2025-11-15 17:10:17 -0300

    feat: using env in datasource

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import dotenv from "dotenv";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'papinha123', // 'admin' no caso de vocês    
-    database: 'cascata',
-    synchronize: false,
+    type: process.env.DATABASE_TYPE as any,
+    port: Number(process.env.DATABASE_PORT),
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: String(process.env.DATABASE_NAME),
+    synchronize: true,
     entities: ['src/models/*.ts'],
-    migrations: ['src/migrations/*.ts'],
+    migrations: ['src/migrations/*.ts']
 });
